d78b6caac387e4694d04e437f4f4f2799f5ca185
Ryusei Yamaguchi <mandel59@gmail.com>
2021-04-09 01:05:42 +0900

    bump up version

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ family = "M+"
 author = "M+ FONTS PROJECT"
 os2_vendor = "M+  "
 url = "http://mplus-fonts.osdn.jp"
-year = "2015"
-version = "1.060"
+year = "2019"
+version = "1.063a"
 
 license = {
   'Japanese': '''\
